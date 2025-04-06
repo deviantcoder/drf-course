bafed1b12adc870b8596600b290fffcdb0c4fc4e
Andrew <andrewsafonovpost@gmail.com>
2025-04-06 18:17:06 +0300

    Added Order/OrderItem serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 
-from api.serializers import ProductSerializer
-from api.models import Product
+from api.serializers import ProductSerializer, OrderSerializer
+from api.models import Product, Order
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -19,5 +19,13 @@ def product_list(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, id=pk)
     serializer = ProductSerializer(product)
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
 
     return Response(serializer.data)
